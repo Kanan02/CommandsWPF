50ac5f0618f3995438044a611a6e4275ad3690e7
Gurb_bx51@itstep.edu.az <Gurb_bx51@itstep.edu.az>
2021-07-15 21:23:07 +0400

    Add KeyBindings

--- Commands/DataSource.cs
+++ Commands/DataSource.cs
@@ -14,12 +14,14 @@ namespace Commands
         Command blueCommand;
         Command redCommand;
         Command greenCommand;
+        Command yellowCommand;
         string selectedColor="Black";
         public DataSource()
         {
             blueCommand = new DelegateCommand(() => SelectedColor = "Blue", () => SelectedColor != "Blue");
             redCommand = new DelegateCommand(() => SelectedColor = "Red", () => SelectedColor != "Red");
             greenCommand = new DelegateCommand(() => SelectedColor = "Green", () => SelectedColor != "Green");
+            yellowCommand = new DelegateCommand(() => SelectedColor = "Yellow", () => SelectedColor != "Yellow");
             PropertyChanged += (sender, e) =>
             {
                 if (e.PropertyName.Equals(nameof(SelectedColor)))
@@ -27,12 +29,14 @@ namespace Commands
                     blueCommand.RiseCanExecuteChange();
                     redCommand.RiseCanExecuteChange();
                     greenCommand.RiseCanExecuteChange();
+                    yellowCommand.RiseCanExecuteChange();
                 }
             };
         }
         public ICommand BlueCommand => blueCommand;
         public ICommand RedCommand => redCommand;
         public ICommand GreenCommand => greenCommand;
+        public ICommand YellowCommand => yellowCommand;
 
         public string SelectedColor { get => selectedColor;
             set
